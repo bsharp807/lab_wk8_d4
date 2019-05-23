2b2fccede7d33bdad90933f88538f8e123a14bfa
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-05-23 14:34:00 +0100

    styling added

--- src/components/Pic.js
+++ src/components/Pic.js
@@ -1,8 +1,9 @@
 import  React from 'react'
+import './picslist.css'
 
 const Pic = (props) => {
   return(
-    <img src={props.url} alt={props.url}/>
+    <img src={props.url} alt={props.url} className='image'/>
   )
 }
 
--- src/components/PicsList.js
+++ src/components/PicsList.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import Pic from './Pic'
+import React from "react";
+import Pic from "./Pic";
+import './picslist.css'
 
-const PicsList = (props) => {
+const PicsList = props => {
+  let i = 1;
+  let column1 = [];
+  let column2 = [];
+  let column3 = [];
+  let column4 = [];
 
-  const mapPics = () => {
-    return(
-      props.doggos.map((element, index) => {
-        return (
-          <Pic
-          url = {element.message}
-          key = {index}
-          />
-        )
-      })
-    )
-  }
+  props.doggos.map((element, index) => {
+    if (i === 1) {
+      column1.unshift(element);
+      i++;
+    } else if (i === 2) {
+      column2.unshift(element);
+      i++;
+    } else if (i === 3) {
+      column3.unshift(element);
+      i++;
+    } else if (i === 4) {
+      column4.unshift(element);
+      i = 1;
+    }
+  });
 
-  return(
+  const mapcolumn1 = () => {
+    console.log(column1);
+    if (column1.length > 0) {
+      return column1.map((element, index) => {
+        return <Pic url={element.message} key={index} />;
+      });
+    }
+    return null;
+  };
+  const mapcolumn2 = () => {
+    if (column2.length > 0) {
+      return column2.map((element, index) => {
+        return <Pic url={element.message} key={index} />;
+      });
+    }
+    return null;
+  };
+  const mapcolumn3 = () => {
+    if (column3.length > 0) {
+      return column3.map((element, index) => {
+        return <Pic url={element.message} key={index} />;
+      });
+    }
+    return null;
+  };
+  const mapcolumn4 = () => {
+    if (column4.length > 0) {
+      return column4.map((element, index) => {
+        return <Pic url={element.message} key={index} />;
+      });
+    }
+    return null;
+  };
+
+  return (
     <div>
-      {mapPics()}
+      <div className='column1'>{mapcolumn1()}</div>
+      <div className='column2'>{mapcolumn2()}</div>
+      <div className='column3'>{mapcolumn3()}</div>
+      <div className='column4'>{mapcolumn4()}</div>
     </div>
-  )
-}
+  );
+};
 
-export default PicsList
+export default PicsList;
